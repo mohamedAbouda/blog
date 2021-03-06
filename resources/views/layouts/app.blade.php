67fadf52aba4f091d03042ef710acf4075c240e6
mohamed abouda <mohamed.aboda500@gmail.com>
2021-03-06 15:06:42 +0200

    travis

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -99,7 +99,7 @@
           </div>
         </div>
       </div>
-    </div>
+    </div>  
   </div>
   <!-- Scripts -->
   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
